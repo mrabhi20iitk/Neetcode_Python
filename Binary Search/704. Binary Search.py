5d193be5afc1ad21aacdae0b8d129fb94e7364d9
Abhishek <mrabhi20iitk@users.noreply.github.com>
2024-02-07 01:00:11 +0530

    Create 704. Binary Search.py

---
+++ Binary Search/704. Binary Search.py
@@ -0,0 +1,10 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l ,r = 0 , len(nums)-1
+        while l<=r:
+            mid =  l + (r-l)//2    # to prevent integer overflow
+            if target == nums[mid]: return mid
+            elif target < nums[mid] : r = mid-1
+            else: l = mid+ 1
+                
+        return -1
