598bffd490e78d42554ebf3c93c17035a7d9a736
Abhishek <mrabhi20iitk@users.noreply.github.com>
2024-02-14 00:10:47 +0530

    Create 219. Contains Duplicate II.py

---
+++ Sliding Window/219. Contains Duplicate II.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        if len(nums) == len(set(nums)):
+            return False
+        
+        for i in range(len(nums)):
+            if len(nums[i:i+k+1]) > len(set(nums[i:i+k+1])):
+                    return True
+                
+        return False
+            
